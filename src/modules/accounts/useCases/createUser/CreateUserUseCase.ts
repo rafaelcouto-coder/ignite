6757641d349e6984d34e0ed92c98ab6c6eb3806a
Rafael Couto <rafaelsimoes133@gmail.com>
2022-04-18 21:52:25 -0300

    tratamento de exceções

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -20,7 +21,7 @@ class CreateUserUseCase {
     const usersAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (usersAlreadyExists) {
-      throw new Error("Users already exists!");
+      throw new AppError("Users already exists!");
     }
 
     const passwordHash = await hash(password, 8);
