af6e4c81834a93537d28c37b60ad7d8dd2b2a7dd
Rafael Couto <rafaelsimoes133@gmail.com>
2022-05-04 22:15:06 -0300

    create migration cars

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
+import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import swaggerUi from "swagger-ui-express";
 import "express-async-errors";
-import "reflect-metadata";
 
 import "@shared/infra/typeorm";
 import "@shared/container";
